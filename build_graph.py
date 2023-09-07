382805db8ac55154d872c64999a4086e53887edc
MrHosse <hgholizadeh8@gmail.com>
2023-09-07 06:46:47 +0200

    new graph data

--- build_graph.py
+++ build_graph.py
@@ -1,40 +1,47 @@
 import networkx as nx
 import random
 import os
+import math
 
 if __name__ == '__main__':
     
-    save_path = 'input_data/5_10_graphs'
-    os.makedirs(save_path, exist_ok=True)
-    with open(save_path + '/README.md', 'w') as readmef:
-        readmef.write(
-            "The graphs in this folder contain 5 groups of 10 nodes.\n" +
-            "Probility of edges in a group: a rondom in [0.75, 1.0]\n" + 
-            "Probility of edges between groups: a random in [0.0, 0.25]"
+    graph_groups = [[1.0,0.0],[0.9,0.1],[0.8,0.2],[0.7,0.3],[0.6,0.4],[0.5,0.5]]
+    
+    for groups in graph_groups:
+        in_group = groups[0]
+        btw_group = groups[1]
+        
+        save_path = 'input_data/5_10_graphs_' + str(in_group) + 'g_' + str(btw_group) + 'ng'
+        os.makedirs(save_path, exist_ok=True)
+        with open(save_path + '/README.md', 'w') as readmef:
+            readmef.write(
+                "The graphs in this folder contain 5 groups of 10 nodes.\n" +
+                "Probility of edges in a group: " + str(in_group) + "\n" + 
+                "Probility of edges between groups: " + str(btw_group)
             )
     
-    # 10 groups of 50
-    sizes = [10 for _ in range(5)]
+        # 5 groups of 10
+        sizes = [10 for _ in range(5)]
     
-    # 50 graphs
-    for n in range(50):
-        probs = []
-        for i in range(5):
-            prob_g_i = []
-            for j in range(5):
-                if i == j:
-                    prob_g_i.append(random.uniform(0.75, 1))
-                else:
-                    prob_g_i.append(random.uniform(0, 0.25))
-            probs.append(prob_g_i)
-        graph = nx.stochastic_block_model(sizes=sizes, p=probs, directed=True)
-        nx.write_edgelist(graph, save_path + '/graph_sample_' + str(n), data=False)
+        # 50 graphs
+        for n in range(50):
+            probs = []
+            for i in range(5):
+                prob_g_i = []
+                for j in range(5):
+                    if i == j:
+                        prob_g_i.append(in_group)
+                    else:
+                        prob_g_i.append(btw_group)
+                probs.append(prob_g_i)
+            graph = nx.stochastic_block_model(sizes=sizes, p=probs, directed=True)
+            nx.write_edgelist(graph, save_path + '/graph_sample_' + str(n), data=False)
         
     
     
     save_path = 'input_data/geometric_graphs'
     os.makedirs(save_path, exist_ok=True)
-    radius = 0.1
+    radius = math.sqrt(0.2 * (1 / math.pi))
     with open(save_path + '/README.md', 'w') as readmef:
         readmef.write(
             "The graphs in this folder contain random geometric graphs\n" +
--- input_data/5_10_graphs/README.md
+++
@@ -1,3 +0,0 @@
-The graphs in this folder contain 5 groups of 10 nodes.
-Probility of edges in a group: a rondom in [0.75, 1.0]
-Probility of edges between groups: a random in [0.0, 0.25]
