b511a486f76fddf9900dcc454c07db0781d72023
usthw <usthw@student.kit.edu>
2023-09-14 01:38:11 +0200

    fix node2vec

--- embedding/node2vec.py
+++ embedding/node2vec.py
@@ -3,10 +3,9 @@ import os
 from subprocess import call
 from subprocess import DEVNULL
 from similarity_metric import InnerProduct
+import networkx as nx
 
 from abstract_embedder import AbstractEmbedder
-
-from gem.utils import graph_util
     
 class Node2Vec(AbstractEmbedder):
 
@@ -16,16 +15,38 @@ class Node2Vec(AbstractEmbedder):
         self._embpath = 'embedding_result/node2vec/'
         self._evlpath = 'evaluation_result/'
         self.similarity_metric = InnerProduct
-        
+    
+    def loadGraphFromEdgeListTxt(file_name, directed=True):
+        with open(file_name, 'r') as f:
+            # n_nodes = f.readline()
+            # f.readline() # Discard the number of edges
+            if directed:
+                G = nx.DiGraph()
+            else:
+                G = nx.Graph()
+            for line in f:
+                edge = line.strip().split()
+                if len(edge) == 3:
+                    w = float(edge[2])
+                else:
+                    w = 1.0
+                G.add_edge(int(edge[0]), int(edge[1]), weight=w)
+        return G
+    
+    def saveGraphToEdgeListTxtn2v(graph, file_name):
+        with open(file_name, 'w') as f:
+            for i, j, w in graph.edges(data='weight', default=1):
+                f.write('%d %d %f\n' % (i, j, w))
+
     def calculate_layout(self, source_graph, dim=4, max_iter=1, walk_len=80, num_walks=10, con_size=10, ret_p=1, inout_p=1):
         
         current_dir = os.path.dirname(os.path.abspath(__file__))
         executable = os.path.abspath(os.path.join(current_dir, 'node2vec_exe/node2vec'))
         
         args = [executable]
-        graph = graph_util.loadGraphFromEdgeListTxt(source_graph)
+        graph = self.loadGraphFromEdgeListTxt(source_graph)
         os.makedirs('embedding/node2vec_exe/' + '/'.join(source_graph.split('/')[:-1]), exist_ok=True)
-        graph_util.saveGraphToEdgeListTxtn2v(graph, os.path.abspath(os.path.join(current_dir, 'node2vec_exe/' + source_graph + '.graph')))
+        self.saveGraphToEdgeListTxtn2v(graph, os.path.abspath(os.path.join(current_dir, 'node2vec_exe/' + source_graph + '.graph')))
         args.append("-i:" + os.path.abspath(os.path.join(current_dir, 'node2vec_exe/' + source_graph + '.graph')))
         args.append("-o:" + os.path.abspath(os.path.join(current_dir, 'node2vec_exe/' + source_graph + '.emb')))
         args.append("-d:%d" % dim)
