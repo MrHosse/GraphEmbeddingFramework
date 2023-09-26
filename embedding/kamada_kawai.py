a2e8eacc82c09341fbd6ac47ae48567b03ef2fa0
usthw <usthw@student.kit.edu>
2023-09-26 21:57:38 +0200

    add kamada kawai

--- embedding/kamada_kawai.py
+++ embedding/kamada_kawai.py
@@ -4,6 +4,7 @@ import time
 
 from abstract_embedder import AbstractEmbedder
 import networkx as nx
+from similarity_metric import EuclidianDistance
 
 class KamadaKawai(AbstractEmbedder):
 
@@ -12,6 +13,7 @@ class KamadaKawai(AbstractEmbedder):
         self._filename = 'embedding/kamada_kawai.py'
         self._embpath = 'embedding_result/kamada_kawai/'
         self._evlpath = 'evaluation_result/'
+        self.similarity_metric = EuclidianDistance
 
     def calculate_layout(self, source_graph):
         
@@ -19,7 +21,8 @@ class KamadaKawai(AbstractEmbedder):
 
         layout = nx.kamada_kawai_layout(graph)
         
-        os.makedirs(self._embpath + 'input_data', exist_ok=True)
+        os.makedirs(self._embpath + '/'.join(source_graph.split('/')[:-1]), exist_ok=True)
+        
         with open(self._embpath + sys.argv[1], 'w') as file:
             output = ""
             for key in layout.keys():
@@ -33,4 +36,7 @@ class KamadaKawai(AbstractEmbedder):
 if __name__ == '__main__':
     kamada_kawai = KamadaKawai()
     
-    kamada_kawai.calculate_layout(source_graph=sys.argv[1])
+    if not os.path.exists(kamada_kawai._embpath + sys.argv[1]):
+        kamada_kawai.calculate_layout(source_graph=sys.argv[1])
+
+    kamada_kawai.save_info()
