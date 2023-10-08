b35310d5624b635d695dfc19b387392a6ecb2473
Hossein Gholizadeh <hgholizadeh8@gmail.com>
2023-10-08 13:59:55 +0100

    add group_based R

--- evaluation/plots/group_based.R
+++ evaluation/plots/group_based.R
@@ -1,38 +1,52 @@
-install.packages("tidyverse")
+# install.packages("tidyverse")
 
 library(tidyverse)
 
 evaluations <- list(
-    list("average_error_link_prediction", "f_score", data.frame()),
-    list("precision_at_k_link_prediction", "pk_ratio", data.frame())
+  list("average_error_link_prediction", "f_score"),
+  list("precision_at_k_link_prediction", "pk_ratio")
 )
 
 folder <- "input_data"
 
-group_names <- list.dirs(path = file.path(getwd(), folder), recursive = FALSE, full.names = FALSE)
-group_paths <- list.dirs(path = file.path(getwd(), folder), recursive = FALSE, full.names = TRUE)
-graph_groups <- Map(c, group_names, group_paths)
+graph_groups <- list.dirs(path = file.path(getwd(), folder), recursive = FALSE, full.names = FALSE)
 
-for (evaluation in evaluations) {
-    metric <- evaluation[1]
-    data_frame <- evaluation[3]
+plots <- list()
 
-    for (group in graph_groups) {
-        name <- group[1]
-        path <- group[2]
-        
-        graphs <- list.dirs(path = path, recursive = FALSE, full.names = TRUE)
+for (i in 1:length(evaluations)) {
+  metric <- evaluations[[i]][1]
+  name <- evaluations[[i]][2]
+  
+  data_frame <- data.frame()
 
-        for (graph in graphs) {
-            results <- read.csv(file.path(graph, paste(metric, ".csv", sep = "")))
-            results$graph_group = paste(name)
-            data_frame <- rbind(data_frame, results)
-        }
+  for (group in graph_groups) {
+    path <- file.path(getwd(), "evaluation_result", folder, group)
+    
+    graphs <- list.dirs(path = path, recursive = FALSE, full.names = TRUE)
+    
+    for (graph in graphs) {
+      results <- read.csv(file.path(graph, paste(metric, ".csv", sep = "")))
+      results$group = paste(group)
+        data_frame <- rbind(data_frame, results)
     }
+  }
+  
+  write.csv(data_frame, file = file.path("evaluation_result", folder, paste(metric, ".csv", sep = "")), row.names = FALSE)
+  
+  #group <- "group"
+  #embedder <- "embedder"
+  #plots[[i]] <- ggplot(data_frame, aes(x = .data[[group]], y = .data[[name]], fill = .data[[embedder]])) +
+    #geom_boxplot()
+  
+  #ggsave(file.path("evaluation_result", folder, paste(metric, ".pdf", sep = "")), width = 16)
 }
 
-for (evaluation in evaluations) {
-  ggplot(evaluation[3], aes(x = graph_group, y = evaluation[2], fill = embedder)) +
-    geom_boxplot()
-  ggsave(file.path("evaluation_result", paste(evaluation[1], ".pdf", sep = "")), width = 16)
-}
+data_frame1 <- read.csv(file.path("evaluation_result", folder, paste("average_error_link_prediction", ".csv", sep = "")))
+ggplot(data_frame1, aes(x = group, y = f_score, fill = embedder)) +
+  geom_boxplot()
+ggsave(file.path("evaluation_result", folder, paste("average_error_link_prediction", ".pdf", sep = "")), width = 16)
+
+data_frame2 <- read.csv(file.path("evaluation_result", folder, paste("precision_at_k_link_prediction", ".csv", sep = "")))
+ggplot(data_frame2, aes(x = group, y = pk_ratio, fill = embedder)) +
+  geom_boxplot()
+ggsave(file.path("evaluation_result", folder, paste("precision_at_k_link_prediction", ".pdf", sep = "")), width = 16)
