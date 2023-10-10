cef75ff0ff1b7bbdbe1a2f422eb2dac8d113dba2
MrHosse <hgholizadeh8@gmail.com>
2023-10-10 11:17:56 +0200

    solve the problem

--- docker/build.sh
+++ docker/build.sh
@@ -5,9 +5,12 @@ docker save -o docker/gra_emb_fw.tar gra_emb_fw
 
 cd docker/
 mkdir gra_emb_fw/
-mkdir gra_emb_fw/input_data
-mkdir gra_emb_fw/embedding_result
-mkdir gra_emb_fw/evaluation_result
+mkdir gra_emb_fw/input_data/
+mkdir gra_emb_fw/embedding_result/
+mkdir gra_emb_fw/evaluation_result/
+mkdir -p gra_emb_fw/embedding/node2vec_exe/
+mkdir -p gra_emb_fw/embedding/struc2vec_exe/temp/
+mkdir -p gra_emb_fw/embedding/verse_exe/temp/
 
 cp gra_emb_fw.tar gra_emb_fw/
 cp enter.sh gra_emb_fw/
