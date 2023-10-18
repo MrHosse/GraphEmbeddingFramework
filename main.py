1212f238d69640719bf15b9502a771c0e3c44347
MrHosse <hgholizadeh8@gmail.com>
2023-10-18 17:41:23 +0200

    add config for main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import run
 import shutil
 import pandas
+import json
 from embedding.spring.spring import Spring
 from embedding.kamada_kawai.kamada_kawai import KamadaKawai
 from embedding.node2vec.node2vec import Node2Vec
@@ -27,9 +28,14 @@ if __name__ == "__main__":
     #run.use_cores(1)
     
     input = 'data/input_data'
+    config_path = 'data/config'
     embedding_result = 'data/embedding_result'
     evaluation_result = 'data/evaluation_result'
     output = 'data/output'
+    
+    os.makedirs(embedding_result, exist_ok=True)
+    os.makedirs(evaluation_result, exist_ok=True)
+    os.makedirs(output, exist_ok=True)
     
     embeddings = list()
     embeddings.append(Spring)
@@ -37,41 +43,53 @@ if __name__ == "__main__":
     embeddings.append(Node2Vec)
     embeddings.append(Struc2Vec)
     embeddings.append(Verse)
-    
-    os.makedirs(embedding_result, exist_ok=True)
-    
-    run.group('layout')
-    for embedding in embeddings:
-        embedding.create_run(getFiles(input), input, embedding_result)
-    
+        
     evaluations = {
         'average_error_link_prediction': ['EuclidianDistance', 'InnerProduct'],
         'precision_at_k_link_prediction': ['EuclidianDistance', 'InnerProduct'],
         'read_time': ['None']
     }
 
-    similarity_metric = {
+    similarity_metrics = {
         'spring': ['EuclidianDistance'],
         'kamada_kawai': ['EuclidianDistance'],
         'node2vec': ['EuclidianDistance'],
         'struc2vec': ['EuclidianDistance'],
         'verse': ['EuclidianDistance']
     }
-
+        
     for embedding_variants in os.listdir(embedding_result):
-        similarity_metric[embedding_variants] = similarity_metric[embedding_variants.split(' ')[0]]
-        similarity_metric[embedding_variants].append('None')
+        similarity_metrics[embedding_variants] = similarity_metrics[embedding_variants.split(' ')[0]]
+        similarity_metrics[embedding_variants].append('None')
     
-    os.makedirs(evaluation_result, exist_ok=True)
+    if os.path.exists(f'{config_path}/main.json'):
+        with(open(f'{config_path}/main.json', 'r')) as config_file:
+            config = json.load(config_file)
+        
+        if config.get('embeddings', None):
+            embeddings = list(map(lambda embedding: globals()[embedding], config['embeddings']))
+        
+        evaluations = config.get('evaluations', evaluations)
+        
+        new_similarity_metrics = config.get('similarity_metrics', dict())
+        for embedding_variants in os.listdir(embedding_result):
+            if not new_similarity_metrics.get(embedding_variants, None):
+                key = embedding_variants.split(' ')[0]
+                new_similarity_metrics[embedding_variants] = new_similarity_metrics.get(key, similarity_metrics.get(key, []))
+        similarity_metrics = new_similarity_metrics
+    
+    run.group('layout')
+    for embedding in embeddings:
+        embedding.create_run(getFiles(input), input, embedding_result)
     
     run.group('evaluation')
-    for embedding in similarity_metric.keys():
+    for embedding in similarity_metrics.keys():
         for evaluation in evaluations.keys():
             run.add(
                 f"evaluate {evaluation}:{embedding}",
                 f"python evaluation/{evaluation}.py \"{embedding_result}/[[embedded_graph]]\" [[sim_metric]]",
                 {'embedded_graph': ['/'.join(path.split('/')[2:]) for path in getFiles(f'{embedding_result}/{embedding}')],
-                'sim_metric': list(set(similarity_metric[embedding]) & set(evaluations[evaluation]))},
+                'sim_metric': list(set(similarity_metrics[embedding]) & set(evaluations[evaluation]))},
                 stdout_file=f'{evaluation_result}/[[embedded_graph]]/[[sim_metric]]/{evaluation}.csv',
             )
 
@@ -81,7 +99,6 @@ if __name__ == "__main__":
     if (os.path.exists('embedding/struc2vec/struc2vec_exe/temp')): shutil.rmtree('embedding/struc2vec/struc2vec_exe/temp')
     if (os.path.exists('embedding/verse/verse_exe/temp')): shutil.rmtree('embedding/verse/verse_exe/temp')
         
-    os.makedirs(output, exist_ok=True)
     graph_groups = os.listdir(input)
     embeddings = [path for path in os.listdir(evaluation_result) if os.path.isdir(f'{evaluation_result}/{path}')]
     all_data_frame = []
